9234d08834cc20895074e5673bdfef818958cf51
sskkfmnn <sskkfmnn@gmail.com>
2023-02-16 15:24:24 +0100

    Lev1_1

---
+++ assets/js/main.js
@@ -0,0 +1,24 @@
+console.log("Hello World");
+
+let a = 12;
+console.log(a);
+
+let b = a*3;
+console.log(b);
+
+console.log("Anton");
+
+let firstName = "Anton";
+console.log(firstName);
+
+let age = 28;
+console.log(age);
+
+let job = "Trainer";
+console.log(job);
+
+let married = true;
+console.log(married);
+
+console.log(firstName + "ist" + age + "Jahre alt" + "von Beruf" + job + "und ist verheiratet" + true);
+
